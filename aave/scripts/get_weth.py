0830f5f8e696c516e78612d6c3e97fe79926f28b
soner <soner.candan@mandg.com>
2022-04-01 10:14:35 +0000

    add eth to weth function

---
+++ aave/scripts/get_weth.py
@@ -0,0 +1,21 @@
+from scripts.helpers import get_account
+from brownie import config, network, interface
+
+
+def main():
+    get_weth()
+
+
+def get_weth():
+    """
+    Get weth using ETH
+    """
+    # weth contract
+    # get ABI , address
+    account = get_account()
+    weth = interface.WethInterface(
+        config["networks"][network.show_active()]["weth_token"]
+    )
+    tx = weth.deposit({"from": account, "value": 0.1 * 10**18})
+    print(f"Received 0.1 WETH")
+    return tx
---
+++ aave/scripts/helpers.py
@@ -0,0 +1,17 @@
+from brownie import network, config, accounts
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork-dev", "mainnet-fork"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache_local"]
+
+
+def get_account(index=0, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    return accounts.add(config["wallets"]["from_key"])
