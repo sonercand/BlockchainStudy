7ccdc88b8d5f5313018de3ecae60680a8731c10a
sonercand <s.ism.candan@gmail.com>
2022-04-01 12:29:47 +0000

    add: approve erc20

--- aave/scripts/aave_borrow.py
+++ aave/scripts/aave_borrow.py
@@ -1,6 +1,10 @@
 from scripts.helpers import get_account
 from scripts.get_weth import get_weth
 from brownie import network, config, interface
+from web3 import Web3
+
+global AMOUNT
+AMOUNT = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -13,7 +17,9 @@ def main():
         get_weth()
     # borrow for aave using ABI and address
     lending_pool = get_lending_pool()
-    print(lending_pool)
+    # print(lending_pool)
+    # approve sending out erc20 tokens
+    approve_erc20(AMOUNT, lending_pool.address, erc20_address, account)
 
 
 def get_lending_pool():
@@ -26,3 +32,13 @@ def get_lending_pool():
     # ABI, address
     lending_pool = interface.ILendingPool(lending_pool_address)
     return lending_pool
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    # ABI, address
+    print("Approving erc20 token")
+    erc20 = interface.IERC20(erc20_address)
+    txn = erc20.approve(spender, amount, {"from": account})
+    txn.wait(1)
+    print("approved")
+    return txn
