70a675403399bc52653b8f53b98c1537d3eee19c
sonercand <s.ism.candan@gmail.com>
2022-04-01 10:54:17 +0000

    add lending interface

--- aave/scripts/aave_borrow.py
+++ aave/scripts/aave_borrow.py
@@ -4,7 +4,18 @@ from brownie import network, config
 
 
 def main():
+    # get account
     account = get_account()
+    # get weth token address
     erc20_address = config["networks"][network.show_active()]["weth_token"]
+    # exchange eth for weth if on mainnet fork
     if network.show_active() == "mainnet-fork-dev":
         get_weth()
+    # borrow for aave using ABI and address
+    lending_pool = get_lending_pool()
+
+
+def get_lending_pool():
+    # lending pool address can change time to time
+    # documentation : https://docs.aave.com/developers/v/1.0/developing-on-aave/the-protocol/lendingpool
+    return None
