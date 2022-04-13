23f482cfec7b235e0859e3b5503f38e1b5426ab0
sonercand <s.ism.candan@gmail.com>
2022-04-13 12:31:24 +0000

    add:kyc contract

--- AssetTokenisation/react/contracts/Sales.sol
+++ AssetTokenisation/react/contracts/Sales.sol
@@ -1,11 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "./Crowdsale.sol";
+import "./KycContract.sol";
 
 contract Sales is Crowdsale {
+    KycContract kyc;
+
     constructor(
         uint256 rate,
         address payable wallet,
-        IERC20 token
-    ) public Crowdsale(rate, wallet, token) {}
+        IERC20 token,
+        KycContract _kyc
+    ) public Crowdsale(rate, wallet, token) {
+        kyc = _kyc;
+    }
+
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount)
+        internal
+        view
+        override
+    {
+        super._preValidatePurchase(beneficiary, weiAmount);
+        require(
+            kyc.kycCompleted(msg.sender),
+            "KYV not completed, purchase not allowed"
+        );
+    }
 }
--- AssetTokenisation/react/scripts/deploy.py
+++ AssetTokenisation/react/scripts/deploy.py
@@ -1,13 +1,16 @@
 from brownie import accounts, network, config
-from brownie import SafeToken, Sales
+from brownie import SafeToken, Sales, KycContract
 from web3 import Web3
 
 
 def deploy():
     account = accounts.add(config["wallets"]["from_key"])
     st = SafeToken.deploy({"from": account}, publish_source=True)
+    kyc = KycContract.deploy()
     print(st.address)
-    sales = Sales.deploy(1, account, st.address, {"from": account}, publish_source=True)
+    sales = Sales.deploy(
+        1, account, st.address, kyc.address, {"from": account}, publish_source=True
+    )
 
 
 def main():
