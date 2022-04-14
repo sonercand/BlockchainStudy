ad7c09164409ab7a6e70832bd17690832fe0dbd8
sonercand <s.ism.candan@gmail.com>
2022-04-14 11:54:01 +0000

    add:basic front end to purchase token

--- AssetTokenisation/frontEnd/token-sales/src/App.js
+++ AssetTokenisation/frontEnd/token-sales/src/App.js
@@ -13,13 +13,17 @@ class App extends React.Component {
     tokenName: '',
     totalSupply: '',
     salesRate: '',
+    saleAddress: '',
     balance: '',
     tokenBalance: '',
     tokenAddress: '',
     kycAddress: "0x123...."
   }
 
+
   async componentDidMount() {
+
+
     const manager = await SafeToken.methods.owner().call();
 
     const tokenName = await SafeToken.methods.name().call();
@@ -29,7 +33,9 @@ class App extends React.Component {
     const tokenBalance = await SafeToken.methods.balanceOf(web3.currentProvider.selectedAddress).call();
     const tokenAddress = map["42"]["SafeToken"][0];
     const salesRate = await Sales.methods.rate_().call();
-    this.setState({ balance, manager, tokenName, totalSupply, tokenBalance, tokenAddress, salesRate });
+    const saleAddress = '0xF9bb5b09079662dA009D85e9405174F1f150701c';
+    this.listenToTokenTransfer();
+    this.setState({ balance, manager, tokenName, totalSupply, tokenBalance, tokenAddress, salesRate, saleAddress }, this.updateUserTokens);
 
   };
 
@@ -39,32 +45,62 @@ class App extends React.Component {
     const name = target.name;
     this.setState({ [name]: value })
   };
-  handleKycWhiteListing = async () => {
+  handleKycWhiteListing = async (event) => {
+    event.preventDefault();
     const accounts = await web3.eth.getAccounts();
     await Kyc.methods.setKycCompleted(this.state.kycAddress).send({ from: accounts[0] });
   };
+  updateUserTokens = async () => {
+    let userTokens = await SafeToken.methods.balanceOf(web3.currentProvider.selectedAddress).call();
+    this.setState({ userTokens: userTokens });
+  };
+  listenToTokenTransfer = async () => {
+    SafeToken.events.Transfer({ to: web3.currentProvider.selectedAddress }).on("data", this.updateUserTokens);
+  };
+  handleBuyTokens = async (event) => {
+    event.preventDefault();
+    await Sales.methods.buyTokens(web3.currentProvider.selectedAddress).send({ from: web3.currentProvider.selectedAddress, value: 10 });
+    const tokenBalance = await SafeToken.methods.balanceOf(web3.currentProvider.selectedAddress).call();
+    const balance = await web3.eth.getBalance(web3.currentProvider.selectedAddress);
+    this.setState({ tokenBalance, balance })
+
+  };
+
+
   render() {
     return (
-      <div>
-        <div>
-          <h2>Read from chain</h2>
-          <p>SafeToken Contract address: {this.state.tokenAddress}</p>
-          <br />
-          <p>{this.state.tokenName} Contract managed by {this.state.manager}</p>
-          <p>Total Supply: {web3.utils.fromWei(this.state.totalSupply)} </p>
+      <div class='row'>
+        <div class='col'>
+          <div class="jumbotron">
+            <h2>Read from chain</h2>
+            <hr />
+            <p>SafeToken Contract address: {this.state.tokenAddress} - <a href='https://kovan.etherscan.io/address/0x121E45ae23d1C34eC119824B86814c58BE0d8Dc3' rel="noreferrer" target='_blank'>Check on etherscan</a>
+            </p>
+            <p>{this.state.tokenName} Contract managed by {this.state.manager}</p>
+            <p>Total Supply: {web3.utils.fromWei(this.state.totalSupply)} </p>
+            <p>Sales Contract Rate: 1 token for 10 wei</p>
+            <p>Your ether balance: {web3.utils.fromWei(this.state.balance)} ether</p>
+            <p> Your token balance: {web3.utils.fromWei(this.state.tokenBalance)}</p>
+            <p> <p>Token sale address : {this.state.saleAddress} <a href='https://kovan.etherscan.io/address/0xF9bb5b09079662dA009D85e9405174F1f150701c' rel="noreferrer" target='_blank'>Check on etherscan</a></p></p>
+          </div>
         </div>
-        <div>Sales Contract Rate: {this.state.salesRate} token per ether
-          <p>balance: {web3.utils.fromWei(this.state.balance)} ether</p>
-          <p> token balance: {web3.utils.fromWei(this.state.tokenBalance)}</p>
-        </div>
-        <div>
-          <h2>Write to chain</h2>
-          <hr />
-          <h3>Allow address </h3>
-          Address to allow: <input type="text" name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputChange} />
-          <button type="button" onClick={this.handleKycWhiteListing}>Allow</button> # ONLY the Owner of the contract can run this.
+        <div class='col'>
+          <div class="jumbotron">
+            <h2>Write to chain</h2>
+            <hr />
+            <h3>Allow address </h3>
+            Address to allow: <input class="form-control" type="text" name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputChange} />
+            <button class='btn btn-primary btn-lg' type="button" onClick={this.handleKycWhiteListing}>Allow</button> # ONLY the Owner of the contract can run this.
+
+
+
+            <p>currently you have {web3.utils.fromWei(this.state.tokenBalance)} tokens</p>
+            <p>Buy one token</p>
+            <p><button class='btn btn-primary btn-lg' type="button" onClick={this.handleBuyTokens}>Buy One Token</button></p>
+          </div>
         </div>
       </div>
+
     )
   }
 };
--- AssetTokenisation/source/scripts/deploy.py
+++ AssetTokenisation/source/scripts/deploy.py
@@ -2,6 +2,8 @@ from brownie import accounts, network, config
 from brownie import SafeToken, Sales, KycContract
 from web3 import Web3
 
+rate = Web3.toWei(0.1, "ether")
+
 
 def deploy():
     account = accounts.add(config["wallets"]["from_key"])
@@ -9,7 +11,7 @@ def deploy():
     kyc = KycContract.deploy({"from": account}, publish_source=True)
     print(st.address)
     sales = Sales.deploy(
-        1, account, st.address, kyc.address, {"from": account}, publish_source=True
+        rate, account, st.address, kyc.address, {"from": account}, publish_source=True
     )
 
 
